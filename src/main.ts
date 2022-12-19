b2274d4f4c38f3fe8da645f56717978dff6ad8d4
William Haywood <will.haywood@vinet.com.au>
2022-12-19 14:40:39 +1100

    Removal of test run - moved to another action

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,16 @@ async function main() {
 		var workspace = process.env.GITHUB_WORKSPACE;
 		core.info(`Building and testing solution (ref: ${context.ref})...`);
 		core.info(`Working directory: ${workspace}...`);
-		core.info("(1/6) Install cross-env");
+		core.info("(1/5) Install cross-env");
 		await exec(`npm i -g cross-env`);
-		core.info("(2/6) Install");
+		core.info("(2/5) Install");
 		await exec(`npm ci`);
 		//Build UAT
-		core.info("(3/6) Build");
+		core.info("(3/5) Build");
 		await exec(`npm run uat-bundle`);
-		core.info("(4/6) Test");
-		await exec(`npm run test`);
-		core.info("(5/6) Package");
+		core.info("(4/5) Package");
 		await exec(`npm run uat-package-solution`);
-		core.info("(6/6) Copy UAT artifact to UAT folder");
+		core.info("(5/5) Copy UAT artifact to UAT folder");
 		//Make UAT folder
 		await mkdirP(`${workspace}\\sharepoint\\solution\\UAT`);
 		//Find sppkg
@@ -38,13 +36,11 @@ async function main() {
 		}
 		core.info(`✅ complete`);
 		//Build PROD
-		core.info("(1/4) Build");
+		core.info("(1/3) Build");
 		await exec(`npm run prod-bundle`);
-		core.info("(2/4) Test");
-		await exec(`npm run test`);
-		core.info("(3/4) Package");
+		core.info("(2/3) Package");
 		await exec(`npm run prod-package-solution`);
-		core.info("(4/4)Copy PROD artifact to PROD folder");
+		core.info("(3/3)Copy PROD artifact to PROD folder");
 		//Find sppkg
 		await mkdirP(`${workspace}\\sharepoint\\solution\\PRODUCTION`);
 		const prodfiles = await globber.glob()
